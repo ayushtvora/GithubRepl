f21c6604e9908d85c4b9cf6c3fdc175fc6f8399d
Ayush Vora <ayushtvora@gmail.com>
2020-10-02 16:44:03 +0000

    combine

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Ayush is very very cool");
-    System.out.println("NO he is not");
+    System.out.println("Vraj is very very cool");
   }
 }
+
+// git checkout master
+// git checkout [branch name] .
