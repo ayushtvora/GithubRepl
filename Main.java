ea0ace9dde96ef20177beca2f38c5abe2d48447b
Vraj Patel - Patel <662465@pdsb.net>
2020-10-02 17:57:50 +0000

    print statement

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Vraj is very very cool");
     System.out.println("and has big hands");
+    System.out.println("Hello");
   }
 }
