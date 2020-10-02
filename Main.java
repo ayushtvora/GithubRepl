7123b44dc1137f13b686a5c5eca726a6b7aeffcb
Ayush Vora <ayushtvora@gmail.com>
2020-10-02 17:21:27 +0000

    yes

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Vraj is very very cool");
     System.out.println("and has big hands");
-    System.out.println("ok now i kinda think ik what im doing");
   }
 }
